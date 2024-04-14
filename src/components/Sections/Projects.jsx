57908fb48dec9a202495d3ec4da34154401f0ce3
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-13 22:05:13 -0400

    Projects Section

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -1,28 +1,51 @@
+import crunchyroll from "../../assets/CrunchyrollHero.png";
+import computron from "../../assets/Computron.png";
+
 function Projects() {
   return (
     <div className="mt-20">
       <ul>
-        <li className="flex hover:border hover:border-purple-500 rounded p-4 mb-4">
-          <p className="w-1/3 text-xxs">IMAGE HERE</p>
-          <div className="flex flex-col w-2/3 gap-3">
-            <p className="text-xs font-bold">Project 1</p>
-            <p className="text-xxs">Description</p>
-          </div>
-        </li>
-        <li className="flex hover:border hover:border-purple-500 rounded p-4 mb-4">
-          <p className="w-1/3 text-xxs">IMAGE HERE</p>
-          <div className="flex flex-col w-2/3 gap-3">
-            <p className="text-xs font-bold">Project 2</p>
-            <p className="text-xxs">Description</p>
-          </div>
-        </li>
-        <li className="flex hover:border hover:border-purple-500 rounded p-4 mb-4">
-          <p className="w-1/3 text-xxs">IMAGE HERE</p>
-          <div className="flex flex-col w-2/3 gap-3">
-            <p className="text-xs font-bold">Project 3</p>
-            <p className="text-xxs">Description</p>
-          </div>
-        </li>
+        <a
+          href="https://computrondevteam.itch.io/computron2-0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <li className="flex gap-4 hover:border hover:border-purple-500 rounded p-4 mb-4">
+            <img
+              className="w-1/4 h-full border border-gray-500 rounded"
+              src={computron}
+              alt="Crunchyroll Hero Image"
+            />
+            <div className="flex flex-col w-2/3 gap-3">
+              <p className="text-xs font-bold">Computron</p>
+              <p className="text-xxs">
+                {`Game that helps people learn coding logic and techniques that is
+                similar to games like "Human Resource Machine" and
+                "7 Billion Humans".`}
+              </p>
+            </div>
+          </li>
+        </a>
+        <a
+          href="https://crunchyroll-landing-page-kz.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <li className="flex gap-4 hover:border hover:border-purple-500 rounded p-4 mb-4">
+            <img
+              className="w-1/4 h-full border border-gray-500 rounded"
+              src={crunchyroll}
+              alt="Crunchyroll Hero Image"
+            />
+            <div className="flex flex-col w-2/3 gap-3">
+              <p className="text-xs font-bold">Crunchyroll Landing Page</p>
+              <p className="text-xxs">
+                Created a landing page to be responsive and compatible with any
+                device.
+              </p>
+            </div>
+          </li>
+        </a>
       </ul>
     </div>
   );
