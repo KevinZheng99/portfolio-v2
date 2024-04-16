fa036aa08b60a9f8a7e2b1aed209c9128e36cc62
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-15 20:15:13 -0400

    Nav link bug fix

--- src/components/Hero/Navigator.jsx
+++ src/components/Hero/Navigator.jsx
@@ -4,12 +4,14 @@ import NavLink from "./NavLink";
 function Navigator() {
   const scrollPosition = useScrollPosition();
 
-  const linkClasses = "group transition-all hover:text-purple-300";
-  const lineClasses =
-    "inline-block w-8 border-t border-gray-500 -translate-y-1 mr-2 transition-all group-hover:w-12 group-hover:border-gray-100";
+  const defaultLinkClasses = "group transition-all";
+  const defaultLineClasses =
+    "inline-block border-t -translate-y-1 mr-2 transition-all group-hover:w-12 group-hover:border-gray-100";
 
-  const activeLinkClasses = linkClasses + " text-purple-300";
-  const activeLineClasses = lineClasses + " border-gray-100 w-12";
+  const activeLinkClasses = defaultLinkClasses + " text-purple-300";
+  const inactiveLinkClasses = defaultLinkClasses + " hover:text-purple-300";
+  const activeLineClasses = defaultLineClasses + " border-gray-100 w-12";
+  const inactiveLineClasses = defaultLineClasses + " border-gray-500 w-8";
 
   return (
     <nav className="text-purple-500 text-xs font-bold mt-12">
@@ -21,12 +23,12 @@ function Navigator() {
             lineStyles={
               scrollPosition >= 0 && scrollPosition < 300
                 ? activeLineClasses
-                : lineClasses
+                : inactiveLineClasses
             }
             linkStyles={
               scrollPosition >= 0 && scrollPosition < 300
                 ? activeLinkClasses
-                : linkClasses
+                : inactiveLinkClasses
             }
           />
         </li>
@@ -37,12 +39,12 @@ function Navigator() {
             lineStyles={
               scrollPosition >= 300 && scrollPosition < 700
                 ? activeLineClasses
-                : lineClasses
+                : inactiveLineClasses
             }
             linkStyles={
               scrollPosition >= 300 && scrollPosition < 700
                 ? activeLinkClasses
-                : linkClasses
+                : inactiveLinkClasses
             }
           />
         </li>
@@ -50,8 +52,12 @@ function Navigator() {
           <NavLink
             name="Projects"
             href="#projects"
-            lineStyles={scrollPosition >= 700 ? activeLineClasses : lineClasses}
-            linkStyles={scrollPosition >= 700 ? activeLinkClasses : linkClasses}
+            lineStyles={
+              scrollPosition >= 700 ? activeLineClasses : inactiveLineClasses
+            }
+            linkStyles={
+              scrollPosition >= 700 ? activeLinkClasses : inactiveLinkClasses
+            }
           />
         </li>
       </ul>
