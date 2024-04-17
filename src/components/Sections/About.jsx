4092565ddec8acba54c1efdc675d916432be2085
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-16 21:15:03 -0400

    updated about section w animation

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <section
       id="about"
-      className="flex flex-col text-xs gap-2 mb-20 scroll-mt-20"
+      className="flex flex-col text-xs gap-2 mb-20 scroll-mt-20 text-slate-400"
     >
       <p>
         My journey into the realm of development began in 2016, captivated by
@@ -11,7 +11,25 @@ function About() {
         calling in software development, particularly focusing on web
         development. Over time, I have been fortunate enough to collaborate with
         exceptional professionals, contributing to the creation of software
-        solutions for a non-profit organization and a start-up.
+        solutions for a{" "}
+        <a
+          className="text-slate-200 font-medium hover:text-purple-500 transition-all"
+          href="https://getaboard.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          non-profit organization
+        </a>{" "}
+        and a{" "}
+        <a
+          className="text-slate-200 font-medium hover:text-purple-500 transition-all"
+          href="https://www.quanwellbeing.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          start-up
+        </a>
+        .
       </p>
       <p>
         I&apos;m currently focusing on imporving my abilities on buliding better
@@ -21,7 +39,50 @@ function About() {
       <p>
         During my off time I love playing basketball with my friends,
         powerlifting, training for a marathon or I&apos;m tending to my crops
-        and animals in Stardew Valley.
+        and animals in{" "}
+        <span className="group text-slate-200 font-medium hover:cursor-junimo">
+          <span className="group-hover:text-red-500 transition-all duration-75 delay-50">
+            S
+          </span>
+          <span className="group-hover:text-orange-500 transition-all duration-75 delay-75">
+            t
+          </span>
+          <span className="group-hover:text-yellow-500 transition-all duration-75 delay-100">
+            a
+          </span>
+          <span className="group-hover:text-green-300 transition-all duration-75 delay-125">
+            r
+          </span>
+          <span className="group-hover:text-green-500 transition-all duration-75 delay-150">
+            d
+          </span>
+          <span className="group-hover:text-teal-500 transition-all duration-75 delay-175">
+            e
+          </span>
+          <span className="group-hover:text-blue-300 transition-all duration-75 delay-200">
+            w
+          </span>
+          <span className="group-hover:text-blue-500 transition-all duration-75 delay-225">
+            {" "}
+            V
+          </span>
+          <span className="group-hover:text-indigo-400 transition-all duration-75 delay-250">
+            a
+          </span>
+          <span className="group-hover:text-indigo-600 transition-all duration-75 delay-275">
+            l
+          </span>
+          <span className="group-hover:text-purple-400 transition-all duration-75 delay-300">
+            l
+          </span>
+          <span className="group-hover:text-purple-600 transition-all duration-75 delay-325">
+            e
+          </span>
+          <span className="group-hover:text-pink-500 transition-all duration-75 delay-350">
+            y
+          </span>
+        </span>
+        .
       </p>
     </section>
   );
